4612c35b1d414c4a687ac125906bae1c733ea32f
FfrftTK <t1610415@uec.ac.jp>
2020-11-20 23:54:48 +0900

    first commit

---
+++ src/main/kotlin/com/ffrfttk/rfid/TagDataManager/TagDataManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.ffrfttk.rfid.TagDataManager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TagDataManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<TagDataManagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/ffrfttk/rfid/TagDataManager/TagDataManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ffrfttk.rfid.TagDataManager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TagDataManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
