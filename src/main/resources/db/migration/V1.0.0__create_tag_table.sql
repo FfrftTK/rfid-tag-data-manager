e88f1cd94f81182cdb06ea4ba95838fe2d5b1984
FfrftTK <t1610415@uec.ac.jp>
2020-11-21 18:19:19 +0900

    Fix migration

--- src/main/resources/db/migration/V1.0.0__create_tag_table.sql
+++ src/main/resources/db/migration/V1.0.0__create_tag_table.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS tags (
-    id SERIAL PRIMARY KEY,
-    tagId VARCHAR (100) NOT NULL,
-    tagName VARCHAR (100),
+    id VARCHAR (100) PRIMARY KEY,
+    name VARCHAR (100),
     description VARCHAR (100)
 );
