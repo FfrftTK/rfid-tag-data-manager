e49efa058a8dfeac541b65cc4540b64c96d1dff4
FfrftTK <t1610415@uec.ac.jp>
2020-11-22 23:52:27 +0900

    Add jwt token create func

--- src/main/kotlin/com/ffrfttk/rfid/TagDataManager/controller/UserController.kt
+++ src/main/kotlin/com/ffrfttk/rfid/TagDataManager/controller/UserController.kt
@@ -1,5 +1,8 @@
 package com.ffrfttk.rfid.TagDataManager.controller
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.exceptions.JWTCreationException
 import com.ffrfttk.rfid.TagDataManager.entity.User
 import com.ffrfttk.rfid.TagDataManager.service.UserService
 import org.springframework.web.bind.annotation.*
@@ -8,6 +11,7 @@ import java.util.*
 @RestController
 @RequestMapping("/api/v1/users")
 class UserController (private val userService: UserService) {
+    val privateKey = "secret"
 
     @GetMapping(path = ["{id}"])
     fun findById(@PathVariable id: Long): Optional<User> {
@@ -16,7 +20,30 @@ class UserController (private val userService: UserService) {
 
     @PostMapping("")
     fun create(@RequestBody user: User): User {
+        val res = createToken(user)
         return userService.save(user)
     }
+
+    // Utils
+    fun createToken(user: User) : String {
+        lateinit var token: String
+        try {
+            val algorithm = Algorithm.HMAC256(privateKey)
+            val calendar = Calendar.getInstance()
+            calendar.time = Date()
+            calendar.add(Calendar.SECOND, 30)
+            val expireTime = calendar.time
+            token = JWT.create()
+                .withIssuer("auth0")
+                .withClaim("name", user.name)
+                .withExpiresAt(expireTime)
+                .sign(algorithm)
+        } catch(e: JWTCreationException){
+            return "Invalid Token"
+        }
+        return token
+    }
+
+
 
 }
