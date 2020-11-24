d786c4df76da22730a9e4c1dfaba3acc39d2a1b1
FfrftTK <t1610415@uec.ac.jp>
2020-11-25 01:14:49 +0900

    Fix user controller

--- src/main/kotlin/com/ffrfttk/rfid/TagDataManager/controller/UserController.kt
+++ src/main/kotlin/com/ffrfttk/rfid/TagDataManager/controller/UserController.kt
@@ -12,7 +12,6 @@ import java.util.*
 @RestController
 @RequestMapping("/api/v1/users")
 class UserController (private val userService: UserService) {
-    val privateKey = "secret"
 
     @GetMapping(path = ["{id}"])
     fun findById(@PathVariable id: Long): Optional<User> {
@@ -24,44 +23,6 @@ class UserController (private val userService: UserService) {
         user.password = BCryptPasswordEncoder()
             .encode(user.passwordRaw)
         return userService.save(user)
-    }
-
-    @PostMapping(path = ["login"])
-    fun login(@RequestBody user: User): User {
-        val res = createToken(user)
-        return userService.save(user)
-    }
-
-    // Utils
-    fun createToken(user: User) : String {
-        lateinit var token: String
-        try {
-            val algorithm = Algorithm.HMAC256(privateKey)
-            val calendar = Calendar.getInstance()
-            calendar.time = Date()
-            calendar.add(Calendar.SECOND, 30)
-            val expireTime = calendar.time
-            token = JWT.create()
-                .withIssuer("auth0")
-                .withClaim("name", user.name)
-                .withExpiresAt(expireTime)
-                .sign(algorithm)
-        } catch(e: JWTCreationException){
-            return "Invalid Token"
-        }
-        return token
-    }
-
-    fun authenticateToken(token: String) {
-        try {
-            val algorithm = Algorithm.HMAC256(privateKey)
-            val jwtVerifier = JWT.require(algorithm)
-                .withIssuer("auth0")
-                .build()
-            jwtVerifier.verify(token)
-        } catch (e: JWTCreationException) {
-            println("Invalid Token")
-        }
     }
 
 }
