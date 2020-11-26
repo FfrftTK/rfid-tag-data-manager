95e881108959a0a0b1362d0243ac6445080660ff
FfrftTK <t1610415@uec.ac.jp>
2020-11-26 20:47:39 +0900

    Fix lint

--- src/main/kotlin/com/ffrfttk/rfid/TagDataManager/security/SecurityProperties.kt
+++ src/main/kotlin/com/ffrfttk/rfid/TagDataManager/security/SecurityProperties.kt
@@ -1,12 +1,12 @@
 package com.ffrfttk.rfid.TagDataManager.security
 
-import com.auth0.jwt.algorithms.Algorithm
 import java.util.concurrent.TimeUnit
+
 
 class SecurityProperties {
     companion object {
         const val SECRET = "secret"
-        val EXPIRATION_TIME = TimeUnit.MINUTES.toMillis(10)
+        val EXPIRATION_TIME = TimeUnit.MINUTES.toMillis(300)
         const val TOKEN_HEADER = "Authorization"
         const val TOKEN_PREFIX = "Bearer "
         const val SIGN_IN_URL = "/login"
